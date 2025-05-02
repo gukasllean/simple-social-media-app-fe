94c89417e68f4226273cc8254b956b2653636c72
Lukas <2381027@unai.edu>
2025-05-03 03:11:38 +0700

    apiaut

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -27,7 +27,7 @@ const RegisterPage = () => {
     setIsSubmitting(true);
     try {
       const response = await axios.post<{ access_token: string }>(
-        `${import.meta.env.VITE_API_URL}/auth/register`,
+        `${import.meta.env.VITE_API_URL}auth/register`,
         data
       );
 
